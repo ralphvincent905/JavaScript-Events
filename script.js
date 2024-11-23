7cfc9e4adf035bc50aa0e495fce0cbd7cb3abc35
ralphvincent905 <ralph_sampaga@yahoo.com>
2024-11-22 21:05:10 -0500

    Add Form Event

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ const keyboardMessage = document.getElementById('keyboardMessage');
 keyboardInput.addEventListener('keyup', (event) => {
     keyboardMessage.textContent = `You pressed: ${event.key}`;
 });
+
+const eventForm = document.getElementById('eventForm');
+const formMessage = document.getElementById('formMessage');
+eventForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    formMessage.textContent = `Form submitted successfully! Hello, ${name || 'Guest'}!`;
+});
